8e949872130301925370c9d0b752d8d50b561a76
Aric Zhuang <ariczhuang@gmail.com>
2018-11-25 21:54:59 -0500

    Updates

---
+++ precache-manifest.5d77749860891deffa82994015ecc367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99e457706928e98b1012",
+    "url": "/spotify-lyrics-app/static/js/runtime~main.99e45770.js"
+  },
+  {
+    "revision": "3841d71694486c9f711c",
+    "url": "/spotify-lyrics-app/static/js/main.3841d716.chunk.js"
+  },
+  {
+    "revision": "dfda8876ff454af983e6",
+    "url": "/spotify-lyrics-app/static/js/1.dfda8876.chunk.js"
+  },
+  {
+    "revision": "3841d71694486c9f711c",
+    "url": "/spotify-lyrics-app/static/css/main.665b2a92.chunk.css"
+  },
+  {
+    "revision": "f128bd13bab40493f49215c098a57fc1",
+    "url": "/spotify-lyrics-app/index.html"
+  }
+];
