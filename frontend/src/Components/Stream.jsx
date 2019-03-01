a2fd0701d0d06ca82de4b821f52cc6822b4e572a
Aric Zhuang <ariczhuang@gmail.com>
2019-03-01 18:43:06 -0500

    update visualizer

--- frontend/src/Components/Stream.jsx
+++ frontend/src/Components/Stream.jsx
@@ -10,6 +10,41 @@ class Stream extends Component {
       icon: "fa fa-play fa-2x",
       marginLeft: "12px"
     };
+    this.createVisualization = this.createVisualization.bin(this);
+  }
+
+  componentDidMount() {
+    this.createVisualization();
+  }
+
+  createVisualization() {
+    let context = new AudioContext();
+    let analyser = context.createAnalyser();
+    let canvas = this.refs.analyzerCanvas;
+    let ctx = canvas.getContext("2d");
+    let audio = this.refs.audio;
+    audio.crossOrigin = "anonymous";
+    let audioSrc = context.createMediaElementSource(audio);
+    audioSrc.connect(analyser);
+    audioSrc.connect(context.destination);
+    analyser.connect(context.destination);
+
+    function renderFrame() {
+      let freqData = new Uint8Array(analyser.frequencyBinCount);
+      requestAnimationFrame(renderFrame);
+      analyser.getByteFrequencyData(freqData);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      console.log(freqData);
+      ctx.fillStyle = "#9933ff";
+      let bars = 100;
+      for (var i = 0; i < bars; i++) {
+        let bar_x = i * 3;
+        let bar_width = 2;
+        let bar_height = -(freqData[i] / 2);
+        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+      }
+    }
+    renderFrame();
   }
 
   setDevice() {
@@ -57,24 +92,21 @@ class Stream extends Component {
   }
   render() {
     return (
-      <div style={{textAlign: "center"}}>
-        <audio id="background">
+      <div style={{ textAlign: "center" }}>
+        <audio ref="audio" id="background">
           <source src="" preload="none" type="audio/mpeg" />
         </audio>
         <br />
         <br />
         <br />
         <br />
-        <button
-          className="button"
-          id="player"
-          onClick={() => this.play()}
-        >
+        <button className="button" id="player" onClick={() => this.play()}>
           <i
             className={this.state.icon}
             style={{ fontSize: "80px", marginLeft: this.state.marginLeft }}
           />
         </button>
+        <canvas ref="analyzerCanvas" id="analyzer" />
       </div>
     );
   }
